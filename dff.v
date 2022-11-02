c2e042ea53dac189374addf4a4194ca278492a90
Bailey Grimes <baileygrimes@Baileys-MacBook-Pro.local>
2022-11-02 11:18:13 -0500

    Refactored DFF

--- dff.v
+++ dff.v
@@ -5,12 +5,12 @@
 `timescale 1ns/10ps
 module dff(clk, rst, en, d, q);
     input clk, rst, en;
-    input[3:0] d;
-    output reg[3:0] q;
+    input[15:0] d;
+    output reg[15:0] q;
 
     always @(posedge clk or posedge rst) begin
         if(rst)
-            q <= 4'b0000;
+            q <= 16'b0000;
         else if(en)
             q <= d;
     end
