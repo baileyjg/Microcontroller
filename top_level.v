4e6d3e433e0d6524ffb0f4d5cea0a39f42d31fae
Bailey Grimes <baileygrimes@Baileys-MacBook-Pro.local>
2022-11-02 11:26:10 -0500

    Begin top-level

---
+++ top_level.v
@@ -0,0 +1,12 @@
+// Micro-Controller Top-Level
+// Author: Bailey Grimes
+// Date: 11/2/22
+
+`include "ALU.v"
+`include "dff.v"
+`include "tri_state.v"
+`timescale 1ns/10ps
+
+module top_level();
+
+endmodule
