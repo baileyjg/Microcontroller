b5e086cc18aa7827f3af0d28bfa6108246c2aa64
Bailey Grimes <baileygrimes@baileys-mbp.ddns.uark.edu>
2022-11-04 11:35:10 -0500

    Update testbench

--- top_level.v
+++ top_level.v
@@ -7,22 +7,19 @@
 `include "tri_state.v"
 `timescale 1ns/10ps
 
-module top_level(clk, rst, bus, opControl, ALUin0, ALUin1, ALUOutLatch, ALUOutEn);
+module top_level(clk, rst, busIn, busOut, opControl, ALUin0, ALUin1, ALUOutLatch, ALUOutEn);
 input clk, rst, ALUin0, ALUin1, ALUOutLatch, ALUOutEn;
 input[2:0] opControl;
-output wire[15:0] bus;
-input[15:0] temp0, temp1, temp2; // Port mapping pin conversions
+input[15:0] busIn;
+output wire[15:0] busOut;
+input[15:0] temp0, temp1, temp2, temp3; // Port mapping pin conversions
 
 // Port mappings
-ALU alu(temp0, temp0, opControl, temp1);
+ALU alu(temp0, temp1, opControl, temp2);
 
-dff flip0(clk, rst, ALUin0, bus, temp0); // Input reg 1
-dff flip1(clk, rst, ALUin1, bus, temp0); // Input reg 2
-dff flip2(clk, rst, ALUOutLatch, temp1, temp2); // Output reg
+dff flip0(clk, rst, ALUin0, busIn, temp0); // Input reg 1
+dff flip1(clk, rst, ALUin1, busIn, temp1); // Input reg 2
+dff flip2(clk, rst, ALUOutLatch, temp2, temp3); // Output reg
 
-tri_state buffer(ALUOutEn, temp2, bus); // Output tri-state
-
-assign #2 bus = 16'b0011010100001011;
-assign #5 bus = 16'b1000001100010000;
-
+tri_state buffer(ALUOutEn, temp3, busOut); // Output tri-state
 endmodule
--- top_level_tb.v
+++ top_level_tb.v
@@ -4,10 +4,11 @@
 module top_level_tb;
     reg clk, rst, ALUin0, ALUin1, ALUOutLatch, ALUOutEn;
     reg[2:0] opControl;
-    wire[15:0] bus;
+    reg[15:0] busIn;
+    wire[15:0] busOut;
 
     // Port mapping of the top_level
-    top_level top(clk, rst, bus, opControl, ALUin0, ALUin1, ALUOutLatch, ALUOutEn);
+    top_level top(clk, rst, busIn, busOut, opControl, ALUin0, ALUin1, ALUOutLatch, ALUOutEn);
 
     initial begin
         $dumpfile("top_level.vcd");
@@ -16,6 +17,7 @@ module top_level_tb;
         // Set initial values
         clk = 0;
         rst = 0;
+        busIn = 16'bzzzzzzzzzzzzzzzz;
         opControl = 3'b000;
         ALUin0 = 0;
         ALUin1 = 0;
@@ -25,18 +27,24 @@ module top_level_tb;
         // Begin stimulating values
         #1
         rst = 1;
-        #10
+        #1
+        rst = 0;
+        #1
+        busIn = 16'b1000110010100001;
+        #6
         opControl = 3'b000;
         ALUin0 = 1;
-        #1
+        #2
         ALUin0 = 0;
         #1
+        busIn = 16'b0001001011001000;
+        #17
         ALUin1 = 1;
-        #1
+        #2
         ALUin1 = 0;
-        #5
+        #18
         ALUOutLatch = 1;
-        #1
+        #2
         ALUOutLatch = 0;
         #1
         ALUOutEn = 1;
