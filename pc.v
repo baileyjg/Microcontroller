16323d23d271cde7ab6364a6d6305182d5ac4394
Bailey Grimes <baileygrimes@baileys-mbp.ddns.uark.edu>
2022-11-07 13:17:18 -0600

    Program counter

---
+++ pc.v
@@ -0,0 +1,13 @@
+`timescale 1ns/10ps
+
+module pc(clk, rst, out);
+input clk, rst;
+output reg[15:0] out;
+
+always @(posedge clk or posedge rst) begin
+    if(rst)
+        out <= 16'b0000000000000000;
+    else
+        out <= out + 1;
+end
+endmodule
--- top_level.v
+++ top_level.v
@@ -8,8 +8,9 @@
 `timescale 1ns/10ps
 
 module top_level(clk, rst, busIn, busOut, opControl, ALUin0, ALUin1, ALUOutLatch, ALUOutEn);
-input clk, rst, ALUin0, ALUin1, ALUOutLatch, ALUOutEn;
-input[2:0] opControl;
+input clk, rst;
+input ALUin0, ALUin1, ALUOutLatch, ALUOutEn; // ALU control signals
+input[2:0] opControl; // ALU opcontrol
 input[15:0] busIn;
 output wire[15:0] busOut;
 input[15:0] temp0, temp1, temp2, temp3; // Port mapping pin conversions
@@ -17,9 +18,9 @@ input[15:0] temp0, temp1, temp2, temp3; // Port mapping pin conversions
 // Port mappings
 ALU alu(temp0, temp1, opControl, temp2);
 
-dff flip0(clk, rst, ALUin0, busIn, temp0); // Input reg 1
-dff flip1(clk, rst, ALUin1, busIn, temp1); // Input reg 2
-dff flip2(clk, rst, ALUOutLatch, temp2, temp3); // Output reg
+dff flip0(clk, rst, ALUin0, busIn, temp0); // ALU input reg 1
+dff flip1(clk, rst, ALUin1, busIn, temp1); // ALU input reg 2
+dff flip2(clk, rst, ALUOutLatch, temp2, temp3); // ALU output reg
 
-tri_state buffer(ALUOutEn, temp3, busOut); // Output tri-state
+tri_state buffer(ALUOutEn, temp3, busOut); // ALU output tri-state
 endmodule
