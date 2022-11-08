e07a560b15429f2cc4d8c12f2d6de06b09fd0f8c
Bailey Grimes <baileygrimes@Baileys-MacBook-Pro.local>
2022-11-07 20:14:04 -0600

    I/O devices

--- top_level.v
+++ top_level.v
@@ -20,6 +20,7 @@ input memEN, memRW; // Memory control signals
 input MARin; // MAR control signals
 input MDRwriteEN, MDRreadEN, MDRout; // MDR control signals
 output MFC; // Mem function complete
+input p0Latch, p0Out, p1Latch, p1Out; // I/O control signals
 
 input[15:0] busIn;
 output wire[15:0] busOut;
@@ -28,6 +29,7 @@ wire[15:0] temp0, temp1, temp2, temp3; // ALU port mapping pin conversions
 wire[15:0] temp4; // PC pin conversions
 wire[15:0] temp5, temp6, temp7, temp8; // General purpose reg pin conversions
 wire[15:0] temp9, temp10, temp11, temp12; // Memory pin conversions
+wire[15:0] p0Data, p1Data, p1temp; // I/O pin conversions
 
 // Port mappings //
 
@@ -42,7 +44,7 @@ tri_state buffer(ALUOutEn, temp3, busOut); // ALU output tri-state
 PC pc(clk, rst, temp4);
 tri_state buffer2(PCOutEn, temp4, busOut);
 
-// General purpose registers
+// General purpose registers //
 dff r0(clk, rst, r0Latch, busIn, temp5);
 tri_state buffer3(r0Out, temp5, busOut);
 
@@ -55,12 +57,25 @@ tri_state buffer5(r2Out, temp7, busOut);
 dff r3(clk, rst, r3Latch, busIn, temp8);
 tri_state buffer6(r3Out, temp8, busOut);
 
-// Memory
+// Memory //
 mem MEM(MFC, memEN, temp9, temp10, memRW, temp11);
 
+// MAR
 dff MAR(clk, rst, MARin, busIn, temp9);
 
+// MDR
 dff MDRwrite(clk, rst, MDRwriteEN, busIn, temp10);
 dff MDRread(clk, rst, MDRreadEN, temp11, temp12);
 tri_state MDRtri(MDRout, temp12, busOut);
+
+// I/O //
+
+// P0
+dff p0(clk, rst, p0Latch, busIn, p0Data);
+tri_state p0tri(p0Out, p0Data, busOut);
+
+// P1
+dff p1(clk, rst, p1Latch, p1Data, p1temp);
+tri_state p1tri(p1Out, p1temp, busOut);
+
 endmodule
