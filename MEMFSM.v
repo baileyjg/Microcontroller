a3d064e88b5dd592038a9a842e31560573bb6e8b
Bailey Grimes <baileygrimes@Baileys-MacBook-Pro.local>
2022-11-23 11:09:56 -0600

    Finish Mem FSM

--- MEMFSM.v
+++ MEMFSM.v
@@ -4,8 +4,8 @@
 
 `timescale 1ns/10ps
 
-module MEMFSM(clk, rst, instruction, done, memEN, marIn, mdrWriteEN, mdrReadEN, mdrOut, RW, rxOut, rxIn, pcInc);
-    input clk, rst;
+module MEMFSM(clk, rst, instruction, done, memEN, marIn, mdrWriteEN, mdrReadEN, mdrOut, RW, rxOut, rxIn, pcInc, MFC);
+    input clk, rst, MFC;
     input[15:0] instruction;
     wire[3:0] opCode = instruction[15:12];
     wire[5:0] param1 = instruction[11:6];
@@ -17,7 +17,7 @@ module MEMFSM(clk, rst, instruction, done, memEN, marIn, mdrWriteEN, mdrReadEN, 
     //States
     reg[3:0] pres_state, next_state;
     parameter st0 = 4'b0000, st1 = 4'b0001, st2 = 4'b0010, st3 = 4'b0011, st4 = 4'b0100, st5 = 4'b0101, st6 = 4'b0110;
-    parameter st7 = 4'b0111, st8 = 4'b1000, st9 = 4'b1001, st10 = 4'b1010, st11 = 4'b1011;
+    parameter st7 = 4'b0111, st8 = 4'b1000, st9 = 4'b1001, st10 = 4'b1010, st11 = 4'b1011, st12 = 4'b1100;
 
     // State register
     always @(posedge clk or posedge rst) begin
@@ -27,6 +27,14 @@ module MEMFSM(clk, rst, instruction, done, memEN, marIn, mdrWriteEN, mdrReadEN, 
             pres_state <= st7;
         else if(pres_state == st2 && opCode == 4'b0011) // Branch to st3 for store operation
             pres_state <= st3;
+        else if(pres_state == st6 && MFC) // Load can move into st7
+            pres_state <= st7;
+        else if(pres_state == st6 && !MFC) // Load must stay in st6
+            pres_state <= st6;
+        else if(pres_state == st5 && !MFC) // Store operation stays in st5 until MFC
+            pres_state <= st5;
+        else if(pres_state == st5 && MFC) // Store operation can move into done state
+            pres_state <= st10;
         else if(opCode == 4'b0010 || opCode == 4'b0011)
             pres_state <= next_state;
         else
@@ -79,28 +87,49 @@ module MEMFSM(clk, rst, instruction, done, memEN, marIn, mdrWriteEN, mdrReadEN, 
                 endcase
             end
             st3: begin // Begin Store operation
-                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
+                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxIn <= 4'b0000;pcInc <= 0;
+                case(param1) // Figure out which gen reg to pull the data from
+                    6'b000000: rxOut <= 4'b1000;
+                    6'b000001: rxOut <= 4'b0100;
+                    6'b000010: rxOut <= 4'b0010;
+                    6'b000011: rxOut <= 4'b0001;
+                    default: rxOut <= 4'b0000;
+                endcase
             end
             st4: begin
-                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
+                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 1;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxIn <= 4'b0000;pcInc <= 0;
+                case(param1) // Figure out which gen reg to pull the data from
+                    6'b000000: rxOut <= 4'b1000;
+                    6'b000001: rxOut <= 4'b0100;
+                    6'b000010: rxOut <= 4'b0010;
+                    6'b000011: rxOut <= 4'b0001;
+                    default: rxOut <= 4'b0000;
+                endcase
             end
             st5: begin
-                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
+                done <= 0;memEN <= 1;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
             end
-            st6: begin
-                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
+            st6: begin // Begin load operation
+                done <= 0;memEN <= 1;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 1;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
             end
-            st7: begin // Begin load operation
-                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
+            st7: begin
+                done <= 0;memEN <= 1;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 1;mdrOut <= 0;RW <= 1;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
             end
             st8: begin
-                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
+                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 1;RW <= 1;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
             end
             st9: begin
-                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
+                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 1;rxOut <= 4'b0000;pcInc <= 0;
+                case(param1) // Figure out which gen reg to load the data into
+                    6'b000000: rxIn <= 4'b1000;
+                    6'b000001: rxIn <= 4'b0100;
+                    6'b000010: rxIn <= 4'b0010;
+                    6'b000011: rxIn <= 4'b0001;
+                    default: rxIn <= 4'b0000;
+                endcase
             end
-            st10: begin
-                done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
+            st10: begin // Load and store operations merge into 2nd to last state
+                done <= 1;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
             end
             st11: begin
                 done <= 0;memEN <= 0;marIn <= 0;mdrWriteEN <= 0;mdrReadEN <= 0;mdrOut <= 0;RW <= 0;rxOut <= 4'b0000;rxIn <= 4'b0000;pcInc <= 0;
