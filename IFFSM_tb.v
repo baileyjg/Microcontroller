7c1b9aa3ab17495dd98cfcfbc57de4dbba978a93
Bailey Grimes <baileygrimes@Baileys-MacBook-Pro.local>
2022-12-01 15:39:47 -0600

    Add IF tb and update instructions in mem

--- IFFSM_tb.v
+++ IFFSM_tb.v
@@ -1,12 +1,15 @@
 `include "IFFSM.v"
+`include "PC.v"
 `timescale 1ns/10ps
 
 module IFFSM_tb;
-    reg clk, rst, MFC, done;
+    reg clk, rst, MFC, done, pcInc;
 
-    wire pcInc, PCoutEN, memEN, marIn, mdrWriteEN, mdReadEN, mdrOut, RW, IRin;
+    wire PCoutEN, memEN, marIn, mdReadEN, mdrOut, RW, IRin;
+    wire[15:0] PCout;
 
     IFFSM fsm(clk, rst, done, MFC, PCoutEN, marIn, memEN, RW, mdReadEN, mdrOut, IRin);
+    PC pc(pcInc, rst, PCout);
 
     initial begin
         $dumpfile("IFFSM.vcd");
@@ -15,10 +18,14 @@ module IFFSM_tb;
         // Set initial values
         clk = 0;
         rst = 0;
+        pcInc = 0;
+        done = 0;
 
         // Begin stimulation of values
         rst = 1;
         #1 rst = 0;
+        done = 0;
+        #1 done = 0;
         #60 MFC = 1;
         #15 MFC = 0;
 
